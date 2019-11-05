79ae591327d49134dfb364a906be35cfc3f772f0
bcrossx <bfcrossley@ucdavis.edu>
2019-11-04 22:08:49 -0800

    struc

--- p3/libuthread/sem.c
+++ p3/libuthread/sem.c
@@ -6,31 +6,53 @@
 #include "thread.h"
 
 struct semaphore {
-	/* TODO Phase 1 */
+	int count = 0;
+  queue_t blocked_queue;
+  queue_t unblocked_queue; //blocked/unblocked to prevent starvation
 };
 
 sem_t sem_create(size_t count)
 {
-	/* TODO Phase 1 */
+	enter_critical_section
+
+  sem_t s = (sem_t) malloc(sizeof(struct semaphore));
+
+  //return pointer to initialized semaphore
+  exit_critical_section
 }
 
 int sem_destroy(sem_t sem)
 {
-	/* TODO Phase 1 */
+  enter_critical_section
+
+  exit_critical_section
 }
 
 int sem_down(sem_t sem)
 {
-	/* TODO Phase 1 */
+  enter_critical_section
+	if(sem == NULL){
+    return -1;
+  }
+  exit_critical_section
 }
 
 int sem_up(sem_t sem)
 {
-	/* TODO Phase 1 */
+  enter_critical_section
+  if(sem == NULL){
+    return -1;
+  }
+  exit_critical_section
 }
 
 int sem_getvalue(sem_t sem, int *sval)
 {
-	/* TODO Phase 1 */
+  enter_critical_section
+
+  exit_critical_section
 }
 
+void tid_enqueue(/* arguments */) {
+  /* code */
+}
